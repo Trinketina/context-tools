8b7a66ff3c9a86ad43f0102b706c72a70aee0620
Tina <143371814+Trinketina@users.noreply.github.com>
2026-03-27 01:32:13 -0500

    Update to 1.3

--- src/main/java/net/trinketina/contexttools/PickTool.java
+++ src/main/java/net/trinketina/contexttools/PickTool.java
@@ -13,6 +13,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.trinketina.contexttools.config.ConfigData;
 
 import java.util.Objects;
 
@@ -25,6 +26,14 @@ public interface PickTool {
 
 
         MinecraftClient client = MinecraftClient.getInstance();
+        if (!client.player.isSneaking() && ConfigData.CONFIG.requireCrouching) {
+            //require crouching if enabled
+            return false;
+        }
+        if (!client.currentScreen.hasControlDown() && ConfigData.CONFIG.requireControlHeldDown) {
+            //require holding control if enabled
+            return false;
+        }
 
         if (client.player.isCreative()) {
             //only do tool swapping in survival
@@ -32,7 +41,8 @@ public interface PickTool {
         }
 
         ItemStack heldItem = client.player.getMainHandStack();
-        if (heldItem == null) {
+        if (heldItem == null && ConfigData.CONFIG.requireToolInHand) {
+            //require tool in hand if enabled
             return false;
         }
 
@@ -50,8 +60,8 @@ public interface PickTool {
             heldHoe = true;
         }
 
-        if (!heldPickaxe && !heldShovel && !heldAxe && !heldHoe) {
-            //if not holding a tool, then skip PickTool
+        if (!heldPickaxe && !heldShovel && !heldAxe && !heldHoe && ConfigData.CONFIG.requireToolInHand) {
+            //require tool in hand if enabled
             return false;
         }
 
